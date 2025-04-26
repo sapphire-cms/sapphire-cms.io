2cc4695204cd8097623ffe1afaa9648b27ae0ec0
Alexei KLENIN <alexei.klenin@gmail.com>
2025-04-26 20:12:10 +0200

    Improve home page

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,13 +2,15 @@ import {Component, Input} from '@angular/core';
 import {NavigationStart, Router, RouterLink} from '@angular/router';
 import {Layout} from '../generated/cms/layout/layout.types';
 import {NgClass} from '@angular/common';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     RouterLink,
-    NgClass
+    NgClass,
+    HttpClientModule,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
@@ -20,11 +22,18 @@ export class NavbarComponent {
 
   public menuOpen = false;
 
-  constructor(router: Router) {
+  public latestVersion = '';
+
+  constructor(router: Router, http: HttpClient) {
     router.events.forEach((event) => {
       if(event instanceof NavigationStart) {
         this.menuOpen = false;
       }
+    });
+
+    const url = 'https://registry.npmjs.org/@sapphire-cms/core/latest';
+    http.get<any>(url).subscribe(data => {
+      this.latestVersion = data.version;
     });
   }
 
