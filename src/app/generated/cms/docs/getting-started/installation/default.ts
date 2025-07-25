9334127e41d316e66eb1e9b966240efa82a9d282
Alexei KLENIN <alexei.klenin@gmail.com>
2025-07-26 01:54:13 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/docs/getting-started/installation/default.ts

--- src/app/generated/cms/docs/getting-started/installation/default.ts
+++ src/app/generated/cms/docs/getting-started/installation/default.ts
@@ -3,6 +3,6 @@ import {Docs} from "../../docs.types";
 export const installation_default: Docs = {
   "title": "Installation",
   "precedent": "docs/getting-started/core-concepts",
-  "next": "docs/usage/csm-config",
+  "next": "docs/usage/cms-config:default",
   "content": "To add Sapphire CMS to existing web project execute:\n\n```shell\nnpm i @sapphire-cms/core @sapphire-cms/node @sapphire-cms/cli @sapphire-cms/codegen\n```"
 };
