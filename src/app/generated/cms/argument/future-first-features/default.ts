ea1ea95c841c7e0ba1024c009b3536feef3303d6
Alexei KLENIN <alexei.klenin@gmail.com>
2025-07-30 23:59:16 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/argument/future-first-features/default.ts

---
+++ src/app/generated/cms/argument/future-first-features/default.ts
@@ -0,0 +1,7 @@
+import {Argument} from "../argument.types";
+
+export const futureFirstFeatures_default: Argument = {
+  "title": "Future-First Features",
+  "icon": "fa-rocket",
+  "content": "Sapphire CMS brings innovations other platforms barely dream of:\n\n- Channel-specific content shaping (social, mobile, email, etc.)\n- On-page visual editing via browser extension or embedded tag.\n- Built-in serverless search engine"
+};
