794b1c158c69306282f06b7fbe995eb0a47fbef8
Alexei KLENIN <alexei.klenin@gmail.com>
2025-07-30 23:59:55 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/argument/truly-headless/default.ts

---
+++ src/app/generated/cms/argument/truly-headless/default.ts
@@ -0,0 +1,7 @@
+import {Argument} from "../argument.types";
+
+export const trulyHeadless_default: Argument = {
+  "title": "Truly Headless",
+  "icon": "fa-unlink",
+  "content": "No frontend bias. Build your presentation with React, Vue, Angular, Svelte, Astro, Next, Nuxt — or just HTML.\n\n<div align=\"right\">\n*Sapphire gives you content. You control the experience.*\n</div>"
+};
