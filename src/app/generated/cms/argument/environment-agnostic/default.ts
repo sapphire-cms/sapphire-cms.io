089c9f723909139959a7ed8d862905cbb03089f7
Alexei KLENIN <alexei.klenin@gmail.com>
2025-07-30 23:58:52 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/argument/environment-agnostic/default.ts

---
+++ src/app/generated/cms/argument/environment-agnostic/default.ts
@@ -0,0 +1,7 @@
+import {Argument} from "../argument.types";
+
+export const environmentAgnostic_default: Argument = {
+  "title": "Environment-Agnostic",
+  "icon": "fa-globe",
+  "content": "Runs anywhere JavaScript can:\n\nCloud, edge, terminal, CI/CD, Node, Bun — even in the browser.\n\n<div align=\"right\">\n*No lock-in. No dependencies. Total freedom.*\n</div>"
+};
