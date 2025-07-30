54c476cf85c60e005791da773111e8b2f5576981
Alexei KLENIN <alexei.klenin@gmail.com>
2025-07-30 23:59:26 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/argument/loved/default.ts

---
+++ src/app/generated/cms/argument/loved/default.ts
@@ -0,0 +1,7 @@
+import {Argument} from "../argument.types";
+
+export const loved_default: Argument = {
+  "title": "Loved by Both Devs and Editors",
+  "icon": "fa-heart",
+  "content": "Developers get control and composability.\n\nContent managers get clean tools and instant editing."
+};
