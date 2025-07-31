5e35932dd341a5471fd01e8a8340667e2c7cfe66
Alexei KLENIN <alexei.klenin@gmail.com>
2025-07-31 21:18:47 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/feature/modular/default.ts

---
+++ src/app/generated/cms/feature/modular/default.ts
@@ -0,0 +1,7 @@
+import {Feature} from "../feature.types";
+
+export const modular_default: Feature = {
+  "title": "Microscopic Core, Modular Design",
+  "icon": "fa-atom",
+  "content": "Start small and grow as you go. All you need is a GitHub account and Docker to start publishing content.\n\nSapphire CMS uses a **tiny** core that can be progressively enhanced with **plug-and-play modules** as your project grows."
+};
