0c05c4c4dacdffac25a7f27c8db0e035bd045127
Alexei KLENIN <alexei.klenin@gmail.com>
2025-07-31 21:28:47 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/feature/search-engine/default.ts

---
+++ src/app/generated/cms/feature/search-engine/default.ts
@@ -0,0 +1,7 @@
+import {Feature} from "../feature.types";
+
+export const searchEngine_default: Feature = {
+  "title": "Serverless Search Engine",
+  "icon": "fa-magnifying-glass",
+  "content": "Integrate **Sapphire Search** — a lightweight, fast, serverless-native search engine built just for Sapphire CMS.\n\nEnable your users to **search content**, without relying on third-party search services."
+};
