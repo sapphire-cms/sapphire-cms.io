b615c97cd99ff6a9fe737423e1e2cd9255e467b1
Alexei KLENIN <alexei.klenin@gmail.com>
2025-07-31 21:15:27 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/argument/low-code/default.ts

--- src/app/generated/cms/argument/low-code/default.ts
+++ src/app/generated/cms/argument/low-code/default.ts
@@ -3,5 +3,5 @@ import {Argument} from "../argument.types";
 export const lowCode_default: Argument = {
   "title": "Low-Code, Low-Config",
   "icon": "fa-sliders-h",
-  "content": "Spin up a full CMS in minutes — no boilerplate.\n\n<div align=\"right\">\n*A few terminal commands and a couple YAML files is all it takes.*\n</div>"
+  "content": "Spin up a full CMS in minutes — no boilerplate.\n\n<div align=\"right\">\n<i>A few terminal commands and a couple YAML files is all it takes.</i>\n</div>"
 };
