8c4cc2cd0f477b8cb5481100606ff011426c5f5c
Alexei KLENIN <alexei.klenin@gmail.com>
2025-07-31 21:19:55 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/feature/serverless/default.ts

---
+++ src/app/generated/cms/feature/serverless/default.ts
@@ -0,0 +1,7 @@
+import {Feature} from "../feature.types";
+
+export const serverless_default: Feature = {
+  "title": "Built for the Serverless World",
+  "icon": "fa-cloud",
+  "content": "Sapphire CMS is a **first-class citizen of the serverless ecosystem**.\n\nDeploy effortlessly to **Vercel, Netlify, Cloudflare Workers, Firebase Functions, AWS Lambda**, and more."
+};
