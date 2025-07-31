4b2f61082ae59cba32a7495289af7de46080b61f
Alexei KLENIN <alexei.klenin@gmail.com>
2025-07-31 21:23:16 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/feature/fast/default.ts

---
+++ src/app/generated/cms/feature/fast/default.ts
@@ -0,0 +1,7 @@
+import {Feature} from "../feature.types";
+
+export const fast_default: Feature = {
+  "title": "Lightning-Fast Content Delivery",
+  "icon": "fa-bolt",
+  "content": "Render and push content directly to **CDNs or edge databases**.\n\nEnjoy near-zero latency delivery that feels like content is **hardcoded into your app**, but with full CMS flexibility."
+};
