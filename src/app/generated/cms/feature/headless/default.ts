2f2f4fc45d5d5283a89cb0eb5831d3466ef196e2
Alexei KLENIN <alexei.klenin@gmail.com>
2025-07-31 21:22:12 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/feature/headless/default.ts

---
+++ src/app/generated/cms/feature/headless/default.ts
@@ -0,0 +1,7 @@
+import {Feature} from "../feature.types";
+
+export const headless_default: Feature = {
+  "title": "Headless & Framework-Agnostic",
+  "icon": "fa-masks-theater",
+  "content": "Use **any frontend you want** — Next.js, Nuxt, Astro, Svelte, or plain HTML.\n\nSapphire CMS delivers content via flexible, decoupled APIs that **fit any stack**."
+};
