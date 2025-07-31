9a57f97aa2bc3cda19675a00ce05392209aa9da5
Alexei KLENIN <alexei.klenin@gmail.com>
2025-07-31 21:35:18 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/feature/visual-editing/default.ts

---
+++ src/app/generated/cms/feature/visual-editing/default.ts
@@ -0,0 +1,7 @@
+import {Feature} from "../feature.types";
+
+export const visualEditing_default: Feature = {
+  "title": "Instant Visual Editing",
+  "icon": "fa-pencil",
+  "content": "Edit your content **live and visually**, right from the page.\n\nUse the Sapphire CMS **Chrome or Firefox plugin** to fix typos or tweak text directly on your production site."
+};
