493c3d459460225f469cda6fd555afb088030144
Alexei KLENIN <alexei.klenin@gmail.com>
2026-04-11 17:52:11 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/docs/deployment/firebase/default.ts

---
+++ src/app/generated/cms/docs/deployment/firebase/default.ts
@@ -0,0 +1,8 @@
+import {Docs} from "../../docs.types";
+
+export const firebase_default: Docs = {
+  "title": "Deploy on Firebase",
+  "precedent": "docs/deployment/node:default",
+  "next": "",
+  "content": "```yaml\nlayers:\n  bootstrap: \"@node\"\n  persistence: \"@github\"\n  admin: \"@rest\"\n  management: \"@rest\"\n  platform: \"@firebase\"\n  security: \"@simple-auth\"\n```"
+};
