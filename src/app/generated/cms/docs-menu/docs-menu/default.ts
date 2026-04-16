2c4729e677631e852261c21ff319b6cca8f507a1
Alexei KLENIN <alexei.klenin@gmail.com>
2026-04-16 20:07:41 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/docs-menu/docs-menu/default.ts

--- src/app/generated/cms/docs-menu/docs-menu/default.ts
+++ src/app/generated/cms/docs-menu/docs-menu/default.ts
@@ -202,14 +202,14 @@ export const docsMenu_default: DocsMenu = {
           "chapters": []
         },
         {
-          "title": "AWS Lambda",
-          "ref": "docs/deployment/firebase:default",
-          "coming-soon": true,
+          "title": "Firebase",
+          "coming-soon": false,
           "chapters": []
         },
         {
-          "title": "Firebase",
-          "coming-soon": false,
+          "title": "AWS Lambda",
+          "ref": "docs/deployment/firebase:default",
+          "coming-soon": true,
           "chapters": []
         },
         {
