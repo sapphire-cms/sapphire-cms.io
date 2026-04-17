609839953020cca8ce98ac305f20e0c96946eec4
Alexei KLENIN <alexei.klenin@gmail.com>
2026-04-17 21:04:11 +0200

    Sapphire CMS: delivering rendered artifact src/app/generated/cms/docs/authentication/username-password/default.ts

---
+++ src/app/generated/cms/docs/authentication/username-password/default.ts
@@ -0,0 +1,8 @@
+import {Docs} from "../../docs.types";
+
+export const usernamePassword_default: Docs = {
+  "title": "Username & Password  ",
+  "precedent": "docs/content-management/manager-ui:default",
+  "next": "docs/integrations/overview:default",
+  "content": "toto"
+};
