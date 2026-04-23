31e15106c1f416271a6b5d86555d5271f45512d5
Alexei KLENIN <alexei.klenin@gmail.com>
2026-04-23 23:34:39 +0200

    Sapphire CMS: delivering rendered artifacts on GitHub:  - src/app/generated/cms/docs-menu/docs-menu/default.ts

---
+++ src/app/generated/cms/docs-menu/docs-menu/default.ts
@@ -0,0 +1,300 @@
+import {DocsMenu} from "../docs-menu.types";
+
+export const docsMenu_default: DocsMenu = {
+  "sections": [
+    {
+      "label": "Getting Started",
+      "topics": [
+        {
+          "title": "Introduction",
+          "ref": "docs/getting-started/introduction:default",
+          "coming-soon": false,
+          "chapters": []
+        },
+        {
+          "title": "Core Concepts",
+          "ref": "docs/getting-started/core-concepts:default",
+          "coming-soon": false,
+          "chapters": []
+        },
+        {
+          "title": "Installation",
+          "ref": "docs/getting-started/installation:default",
+          "coming-soon": false,
+          "chapters": []
+        }
+      ]
+    },
+    {
+      "label": "Usage",
+      "topics": [
+        {
+          "title": "CMS Configuration",
+          "ref": "docs/usage/cms-config:default",
+          "coming-soon": false,
+          "chapters": []
+        },
+        {
+          "title": "Content Schemas",
+          "ref": "docs/usage/content-schemas:default",
+          "coming-soon": false,
+          "chapters": []
+        },
+        {
+          "title": "Rendering Pipelines",
+          "ref": "docs/usage/rendering-pipelines:default",
+          "coming-soon": false,
+          "chapters": []
+        },
+        {
+          "title": "Document Shapers",
+          "ref": "docs/usage/document-shapers:default",
+          "coming-soon": false,
+          "chapters": []
+        },
+        {
+          "title": "AOT Bundling",
+          "ref": "docs/usage/aot:default",
+          "coming-soon": false,
+          "chapters": []
+        }
+      ]
+    },
+    {
+      "label": "Content Modeling",
+      "topics": [
+        {
+          "title": "Field Types",
+          "ref": "docs/content-modeling/field-types:default",
+          "coming-soon": false,
+          "chapters": [
+            {
+              "title": "Check",
+              "ref": "docs/content-modeling/field-types/check:default",
+              "coming-soon": false
+            },
+            {
+              "title": "Group",
+              "ref": "docs/content-modeling/field-types/group:default",
+              "coming-soon": false
+            },
+            {
+              "title": "Number",
+              "ref": "docs/content-modeling/field-types/number:default",
+              "coming-soon": false
+            },
+            {
+              "title": "Reference",
+              "ref": "docs/content-modeling/field-types/reference:default",
+              "coming-soon": false
+            },
+            {
+              "title": "Rich Text",
+              "ref": "docs/content-modeling/field-types/rich-text:default",
+              "coming-soon": false
+            },
+            {
+              "title": "Tag",
+              "ref": "docs/content-modeling/field-types/tag:default",
+              "coming-soon": false
+            },
+            {
+              "title": "Text",
+              "ref": "docs/content-modeling/field-types/text:default",
+              "coming-soon": false
+            }
+          ]
+        },
+        {
+          "title": "Field Validators",
+          "ref": "docs/content-modeling/field-validators:default",
+          "coming-soon": false,
+          "chapters": [
+            {
+              "title": "Between",
+              "ref": "docs/content-modeling/field-validators/between:default",
+              "coming-soon": false
+            },
+            {
+              "title": "Integer",
+              "ref": "docs/content-modeling/field-validators/integer:default",
+              "coming-soon": false
+            },
+            {
+              "title": "Required",
+              "ref": "docs/content-modeling/field-validators/required:default",
+              "coming-soon": false
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "label": "Content Management",
+      "topics": [
+        {
+          "title": "CLI",
+          "ref": "docs/content-management/cli:default",
+          "coming-soon": false,
+          "chapters": []
+        },
+        {
+          "title": "REST API",
+          "ref": "docs/content-management/rest:default",
+          "coming-soon": false,
+          "chapters": []
+        },
+        {
+          "title": "Manager UI",
+          "ref": "docs/content-management/manager-ui:default",
+          "coming-soon": false,
+          "chapters": []
+        },
+        {
+          "title": "Live Editor",
+          "ref": "",
+          "coming-soon": true,
+          "chapters": []
+        }
+      ]
+    },
+    {
+      "label": "Authentication",
+      "topics": [
+        {
+          "title": "Username & Password",
+          "ref": "docs/authentication/username-password:default",
+          "coming-soon": false,
+          "chapters": []
+        }
+      ]
+    },
+    {
+      "label": "Integrations",
+      "topics": [
+        {
+          "title": "Overview",
+          "ref": "docs/integrations/overview:default",
+          "coming-soon": false,
+          "chapters": []
+        },
+        {
+          "title": "GitHub",
+          "ref": "docs/integrations/github:default",
+          "coming-soon": false,
+          "chapters": []
+        }
+      ]
+    },
+    {
+      "label": "Deployment",
+      "topics": [
+        {
+          "title": "Overview",
+          "ref": "docs/deployment/deployment-overview:default",
+          "coming-soon": false,
+          "chapters": []
+        },
+        {
+          "title": "Node",
+          "ref": "docs/deployment/node:default",
+          "coming-soon": false,
+          "chapters": []
+        },
+        {
+          "title": "Firebase",
+          "ref": "docs/deployment/firebase:default",
+          "coming-soon": false,
+          "chapters": []
+        },
+        {
+          "title": "AWS Lambda",
+          "coming-soon": true,
+          "chapters": []
+        },
+        {
+          "title": "Bun",
+          "ref": "",
+          "coming-soon": true,
+          "chapters": []
+        },
+        {
+          "title": "Vercel",
+          "ref": "",
+          "coming-soon": true,
+          "chapters": []
+        },
+        {
+          "title": "Browser",
+          "ref": "",
+          "coming-soon": true,
+          "chapters": []
+        }
+      ]
+    },
+    {
+      "label": "Development",
+      "topics": [
+        {
+          "title": "Architecture",
+          "ref": "",
+          "coming-soon": true,
+          "chapters": []
+        },
+        {
+          "title": "Modules",
+          "ref": "",
+          "coming-soon": true,
+          "chapters": []
+        },
+        {
+          "title": "Layers",
+          "ref": "",
+          "coming-soon": true,
+          "chapters": [
+            {
+              "title": "Admin Layer",
+              "ref": "",
+              "coming-soon": true
+            },
+            {
+              "title": "Bootstrap Layer",
+              "ref": "",
+              "coming-soon": true
+            },
+            {
+              "title": "Content Layer",
+              "ref": "",
+              "coming-soon": true
+            },
+            {
+              "title": "Delivery Layer",
+              "ref": "",
+              "coming-soon": true
+            },
+            {
+              "title": "Management Layer",
+              "ref": "",
+              "coming-soon": true
+            },
+            {
+              "title": "Persistence Layer",
+              "ref": "",
+              "coming-soon": true
+            },
+            {
+              "title": "Platform Layer",
+              "ref": "",
+              "coming-soon": true
+            },
+            {
+              "title": "Render Layer",
+              "ref": "",
+              "coming-soon": true
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
