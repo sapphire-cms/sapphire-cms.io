c786fe22c4d0cb282bd7ec33e9a8066eb4f3ca87
Alexei KLENIN <alexei.klenin@gmail.com>
2026-04-24 21:27:17 +0200

    Sapphire CMS: delivering rendered artifacts on GitHub:  - src/app/generated/cms/docs-menu/docs-menu/default.ts

--- src/app/generated/cms/docs-menu/docs-menu/default.ts
+++ src/app/generated/cms/docs-menu/docs-menu/default.ts
@@ -3,7 +3,7 @@ import {DocsMenu} from "../docs-menu.types";
 export const docsMenu_default: DocsMenu = {
   "sections": [
     {
-      "label": "Getting Started",
+      "label": "Getting Started-test-publish",
       "topics": [
         {
           "title": "Introduction",
