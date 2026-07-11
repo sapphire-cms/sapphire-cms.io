3f01c61b39a14b4ffec7b9fea29b160937fd7f47
Alexei KLENIN <alexei.klenin@gmail.com>
2026-07-11 19:10:35 +0200

    Sapphire CMS: delivering rendered artifacts on GitHub:  - src/app/generated/cms/docs/deployment/deployment-overview/default.ts

--- src/app/generated/cms/docs/deployment/deployment-overview/default.ts
+++ src/app/generated/cms/docs/deployment/deployment-overview/default.ts
@@ -2,7 +2,7 @@ import {Docs} from "../../docs.types";
 
 export const deploymentOverview_default: Docs = {
   "title": "Deployment",
-  "precedent": "docs/integrations/github:default",
+  "precedent": "docs/integrations/cloudinary:default",
   "next": "docs/deployment/node:default",
   "content": "Sapphire is designed to be deployable across the widest range of platforms. Its motto is: **\"Runs anywhere JavaScript can.\"**\n\nThis section presents the available official integrations with various platforms, including popular serverless providers."
 };
