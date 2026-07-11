7183ef50477e19f4b8da5082048aa539a9feaf24
Alexei KLENIN <alexei.klenin@gmail.com>
2026-07-11 19:02:12 +0200

    Sapphire CMS: delivering rendered artifacts on GitHub:  - src/app/generated/cms/docs/integrations/cloudinary/default.ts

---
+++ src/app/generated/cms/docs/integrations/cloudinary/default.ts
@@ -0,0 +1,8 @@
+import {Docs} from "../../docs.types";
+
+export const cloudinary_default: Docs = {
+  "title": "Cloudinary",
+  "precedent": "docs/integrations/github:default",
+  "next": "docs/deployment/deployment-overview:default",
+  "content": "TODO"
+};
