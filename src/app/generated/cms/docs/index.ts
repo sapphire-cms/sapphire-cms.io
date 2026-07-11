eb082395b663a59b354adb913135bb78e4503d7c
Alexei KLENIN <alexei.klenin@gmail.com>
2026-07-11 19:56:03 +0200

    Sapphire CMS: delivering rendered artifacts on GitHub:  - src/app/generated/cms/docs/docs.types.ts  - src/app/generated/cms/docs/deployment/firebase/index.ts  - src/app/generated/cms/docs/integrations/cloudinary/index.ts  - src/app/generated/cms/docs/integrations/github/index.ts  - src/app/generated/cms/docs/deployment/deployment-overview/index.ts  - src/app/generated/cms/docs/getting-started/core-concepts/index.ts  - src/app/generated/cms/docs/index.ts

--- src/app/generated/cms/docs/index.ts
+++ src/app/generated/cms/docs/index.ts
@@ -2,3 +2,4 @@ export * from "./deployment/firebase";
 export * from "./integrations/cloudinary";
 export * from "./integrations/github";
 export * from "./deployment/deployment-overview";
+export * from "./getting-started/core-concepts";
