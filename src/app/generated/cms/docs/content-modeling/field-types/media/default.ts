069a3766853560835e9f950b58066ef64a6e9d56
Alexei KLENIN <alexei.klenin@gmail.com>
2026-07-16 23:57:34 +0200

    Sapphire CMS: delivering rendered artifacts on GitHub:  - src/app/generated/cms/docs/content-modeling/field-types/media/default.ts

---
+++ src/app/generated/cms/docs/content-modeling/field-types/media/default.ts
@@ -0,0 +1,8 @@
+import {Docs} from "../../../docs.types";
+
+export const media_default: Docs = {
+  "title": "Media",
+  "precedent": "docs/content-modeling/field-types/text:default",
+  "next": "docs/content-modeling/field-validators/between:default",
+  "content": "TODO"
+};
