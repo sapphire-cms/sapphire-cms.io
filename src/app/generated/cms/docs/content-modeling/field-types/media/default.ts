fa0106a8e07b30c22cee4f2b3746fdc53a25d8fb
Alexei KLENIN <alexei.klenin@gmail.com>
2026-07-17 21:46:27 +0200

    Sapphire CMS: delivering rendered artifacts on GitHub:  - src/app/generated/cms/docs/content-modeling/field-types/media/default.ts

--- src/app/generated/cms/docs/content-modeling/field-types/media/default.ts
+++ src/app/generated/cms/docs/content-modeling/field-types/media/default.ts
@@ -1,8 +1,8 @@
 import {Docs} from "../../../docs.types";
 
 export const media_default: Docs = {
-  "title": "Media",
+  "title": "Media Type",
   "precedent": "docs/content-modeling/field-types/text:default",
   "next": "docs/content-modeling/field-validators:default",
-  "content": "TODO"
+  "content": "The `media` type allows you to reference images and videos managed by the CMS from within your documents.\n\nDuring rendering, `media` fields are resolved into publicly accessible URLs pointing to the corresponding media assets.\n\nExample:\n\n```yaml\nfields:\n  - name: banner\n    label: Banner image\n    required: false\n    type: media\n```"
 };
